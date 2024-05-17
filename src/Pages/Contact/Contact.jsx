8e74e64bc450a804e02b53c0cc92849a500853df
jillurcf <jrtushar@gmail.comm>
2024-05-17 11:03:48 -0700

    sweet alert2 installed

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Contact = () => {
   const form = useRef();
@@ -7,18 +8,35 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_j2oq3uk', 'template_tlb7ati', form.current, 'BvQpcWpomjD6XLlDM')
-    .then((result) => {
-        console.log(result);
-    }, (error) => {
-        console.log(error);
-    });
+    emailjs
+      .sendForm(
+        "service_j2oq3uk",
+        "template_tlb7ati",
+        form.current,
+        "BvQpcWpomjD6XLlDM"
+      )
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      Swal.fire({
+        position: "middle",
+        icon: "success",
+        title: "Your message has sent",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      form.current.reset();
+      
   };
   return (
     <div>
       <div className="hero min-h-screen bg-gray-400">
         <div className="hero-content">
-          
           <div className="card shrink-0 w-full max-w-xs shadow-2xl bg-gray-400">
             <form className="card-body" ref={form} onSubmit={sendEmail}>
               <div className="form-control">
@@ -44,7 +62,6 @@ const Contact = () => {
                   className="input input-bordered"
                   required
                 />
-               
               </div>
               <div className="form-control">
                 <label className="label">
@@ -57,11 +74,9 @@ const Contact = () => {
                   className="input input-bordered h-48"
                   required
                 />
-               
               </div>
-              <div className="form-control mt-6 btn rounded-lg">
-              <input type="submit" value="Send" />
-                {/* <button className="btn btn-primary">Submit</button> */}
+              <div className="form-control">
+                <button className="btn btn-ghost w-full">Submit</button>
               </div>
             </form>
           </div>
