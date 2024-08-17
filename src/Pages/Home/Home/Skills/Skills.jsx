b3972db52e4063e129dcb67fb23f876325d23ec9
jillurcf <jrtushar@gmail.comm>
2024-08-17 09:36:07 -0700

    resume and skill updated

--- src/Pages/Home/Home/Skills/Skills.jsx
+++ src/Pages/Home/Home/Skills/Skills.jsx
@@ -1,13 +1,17 @@
 import html from "../../../../assets/skills/html.svg";
 import css from "../../../../assets/skills/css.svg";
 import js from "../../../../assets/skills/javaScript.svg";
+import ts from "../../../../assets/skills/TypeScript.png";
 import react from "../../../../assets/skills/react.svg";
+import reactNative from "../../../../assets/skills/ReactNative.png";
+import tailwind from "../../../../assets/skills/tailwind.png";
 import bootstrap from "../../../../assets/skills/bootstrap-solid.png";
 import git from "../../../../assets/skills/Git-Icon-1788C.png";
 import figma from "../../../../assets/skills/figma-1.png";
 import node from "../../../../assets/skills/nodejs.png";
 import mongo from "../../../../assets/skills/Vector.png";
 import next from "../../../../assets/skills/next.jpg";
+import web3 from "../../../../assets/skills/web3.png";
 import photoshop from "../../../../assets/skills/photoshop.png";
 import illustrator from "../../../../assets/skills/illustrator.png";
 const Skills = () => {
@@ -35,8 +39,24 @@ const Skills = () => {
               <p>JAVASCRIPT</p>
             </div>
             <div>
+              <img className="mx-auto py-2" src={ts} alt="" />
+              <p>TYPESCRIPT</p>
+            </div>
+            <div>
               <img className="mx-auto py-2" src={react} alt="" />
               <p>REACT</p>
+            </div>
+            <div>
+              <img className="mx-auto py-2" src={reactNative} alt="" />
+              <p>REACT NATIVE</p>
+            </div>
+            <div className="">
+              <img className="mx-auto py-2" src={next} alt="" />
+              <p className="">Next Js</p>
+            </div>
+            <div>
+              <img className="mx-auto py-2" src={tailwind} alt="" />
+              <p>TAILWIND</p>
             </div>
             <div>
               <img className="mx-auto py-2" src={bootstrap} alt="" />
@@ -63,9 +83,10 @@ const Skills = () => {
         <div className="py-12">
           <h1 className="font-bold text-2xl">LEARNING:</h1>
           <div className="grid lg:grid-cols-4 grid-cols-1 gap-12 text-center">
-            <div className="">
-              <img className="mx-auto py-2" src={next} alt="" />
-              <p className="">Next Js</p>
+            <div className="relative">
+              <img className="mx-auto py-2" src={web3} alt="" />
+              <p >WEB</p>
+              <p className="absolute xl:lg:top-[82%] xl:left-[63%]  lg:top-[76%] lg:left-[66%] top-[82%] left-[62%]">3</p>
             </div>
           </div>
         </div>
